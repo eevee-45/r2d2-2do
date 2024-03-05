767c6e662aa2c8faee0c9cd81c0751fe0f7b48fd
Dillon <dillonhale2026@gmail.com>
2024-03-05 12:23:05 -0500

    Some Dashboard buttons created!

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -3,10 +3,16 @@ import { UserButton } from "@clerk/nextjs";
 export default function TodosPage() {
   return (
     <div>
-      <p>Welcome to the r2d2-2do</p>
-      <div className="h-screen">
-        <UserButton />
+      
+      <div className='flex p-10'>
+      <a href='/' className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'>r2d2-2do</a>
+      <div className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'>
+        <UserButton afterSignOutUrl="/"/>
       </div>
+      </div>
+      <main className='flex min-h-screen flex-col items-center justify-between p-24 pt-0'>
+        i am main
+      </main>
     </div>
   );
 }

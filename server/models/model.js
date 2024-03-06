5efd61688b84c57856321809e94e9d200b314883
cyburns <72774499+cyburns@users.noreply.github.com>
2024-03-06 14:18:53 -0500

    crud

--- server/models/model.js
+++ server/models/model.js
@@ -1,22 +1,21 @@
-import pg from 'pg';
+import pg from "pg";
 const { Pool } = pg;
 
-const PG_URI =process.env.PG_URI;
+const PG_URI = process.env.PG_URI;
 
 const pool = new Pool({
-  connectionString: PG_URI
+  connectionString: PG_URI,
 });
 
 export const queryObject = {
   query: async (text, params, callback) => {
-    try{
-      console.log('executed query', text);
+    try {
+      console.log("executed query", text);
       const result = await pool.query(text, params);
-      console.log('reached query');
+      console.log("reached query");
       return result.rows;
-    }catch(error){
+    } catch (error) {
       return error;
     }
-     
-  }
+  },
 };
